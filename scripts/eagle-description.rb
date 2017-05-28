52ee535d01bbbc28efb09c444f2e15a5e285c306
Roman Senn <roman.l.senn@gmail.com>
2017-05-28 02:17:26 +0200

    ...

--- scripts/eagle-description.rb
+++ scripts/eagle-description.rb
@@ -50,42 +50,56 @@ def format_html(description)
  }.join("<br/>\n")
 end
 
-begin
-  if ARGV.length > 0 then
-	@file_name = ARGV[0]
-  else
-	@file_name = "C:/Users/roman/Documents/an-tronics/eagle/AGC-Amplifier-LM13600-Stereo.sch"
-  end
-  if ARGV.length > 1 then
-	@description = ARGV[1]
-  end
-  
-  @doc = Nokogiri::XML(File.open(@file_name))
-  
-  elems = @doc.xpath("/eagle/drawing/*/description")
-  elems.each do |e|
-	pte = PlainTextExtractor.new(true)
-	pte << e.content
+@eagle_files = ARGV.find_all { |a| a.end_with?(".sch", ".brd") }
+@description = ARGV.find_all { |a| not a.end_with?(".sch", ".brd") }.join("\n").gsub("\\n", "\n")
+
+#pp @eagle_files, @description
+
+#  if ARGV.length > 0 then
+#	@file_name = ARGV[0]
+#  else
+#	@file_name = "C:/Users/roman/Documents/an-tronics/eagle/AGC-Amplifier-LM13600-Stereo.sch"
+#  end
+#  if ARGV.length > 1 then
+#	@description = ARGV[1]
+#  end
+#  
+
+@eagle_files.each do |file_name|
+
+#  STDERR.puts "Processing '#{file_name}' ... "
+
+  begin
+	@doc = Nokogiri::XML(File.open(file_name))
 	
-	puts "#{e.path}: '#{pte.plaintext}'"
-	
-	if @description != "" then
-	  e.content = format_html(@description)
+	elems = @doc.xpath("/eagle/drawing/*/description")
+	elems.each do |e|
+	  pte = PlainTextExtractor.new(true)
+	  pte << e.content
+
+      text = pte.plaintext.gsub("\n", "\\n")
+	  
+	  puts "#{file_name}: [#{e.path}] '#{text}'"
+	  
+	  if @description != "" then
+		e.content = format_html(@description)
+	  end
 	end
-  end
 
-  if @description != "" then
-	to = @file_name+".bak"
-	STDERR.puts "Renaming '#{@file_name}' to '#{to}' ..."
+	if @description != "" then
+	  to = file_name+".bak"
+	  STDERR.puts "Renaming '#{file_name}' to '#{to}' ..."
+	  
+	  File.rename(file_name, to);
+
+	  STDERR.puts "Writing XML data to '#{file_name}' ..."
+	  File.write(file_name, @doc.to_xml);
+	end
 	
-	File.rename(@file_name, to);
-
-	STDERR.puts "Writing XML data to '#{@file_name}' ..."
-	File.write(@file_name, @doc.to_xml);
+  rescue SystemCallError => err
+	STDERR.puts "System call error: #{err}"
+  rescue Exception => e
+	STDERR.puts "Exception: #{e}"
   end
-  
-rescue SystemCallError => err
-  STDERR.puts "System call error: #{err}"
-rescue Exception => e
-  STDERR.puts "Exception: #{e}"
+
 end
