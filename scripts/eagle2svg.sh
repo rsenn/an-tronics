228614a73f000cb1bb8ae3ef3c07b80439b20ddc
Roman Senn <roman.l.senn@gmail.com>
2017-06-10 08:22:36 +0200

    ...

--- scripts/eagle2svg.sh
+++ scripts/eagle2svg.sh
@@ -190,8 +190,10 @@ eagle_print() {
     )
     
     svg_set_size "${BASE}.svg" 595.27559 841.88976
-    exec_cmd INKSCAPE --verb=EditSelectAll --verb=AlignHorizontalCenter --verb=AlignVerticalCenter --verb=FileSave --verb=FileQuit "${BASE}.svg"
+    exec_cmd INKSCAPE --export-area-page --verb=EditSelectAll --verb=AlignHorizontalCenter --verb=AlignVerticalCenter --verb=FileSaveAs:"${BASE}.pdf" --verb=FileQuit "${BASE}.svg"
 
+      
+      
       
 #  exec_cmd PDFTOCAIRO -svg  "$FILE" "${FILE%.*}.svg" || exit $?
 #
